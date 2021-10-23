d7b71c477883f91e52c84f62d6bb2aa62502b654
user202729 <25191436+user202729@users.noreply.github.com>
2021-10-23 21:30:54 +0700

    window_class return empty if there's an exception

--- getactivewindow/__init__.py
+++ getactivewindow/__init__.py
@@ -3,7 +3,7 @@ import subprocess
 import ast
 from typing import Union
 
-from xdo import Xdo
+from xdo import Xdo, XdoException
 x = Xdo()
 
 def active_window_id()->int:
@@ -13,6 +13,9 @@ def window_name(window_id: int)->str:
 	return x.get_window_name(window_id).decode('u8')
 
 def window_class(window_id: int)->tuple[str, ...]:
-	result=bytes(x.get_window_property(window_id, b"WM_CLASS")).split(b"\0")
+	try:
+		result=bytes(x.get_window_property(window_id, b"WM_CLASS")).split(b"\0")
+	except XdoException:
+		return ()
 	assert not result[-1]
 	return tuple(x.decode('u8') for x in result[:-1])
