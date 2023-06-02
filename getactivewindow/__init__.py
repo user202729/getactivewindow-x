8164b8263d52db7c11f737bd4084aab5cc375d27
user202729 <25191436+user202729@users.noreply.github.com>
2023-06-02 22:20:27 +0700

    Fix longstanding bug...

--- getactivewindow/__init__.py
+++ getactivewindow/__init__.py
@@ -2,20 +2,39 @@ from dataclasses import dataclass
 import subprocess
 import ast
 from typing import Union
+import threading
 
-from xdo import Xdo, XdoException
-x = Xdo()
+from Xlib import X, display  # type: ignore
+
+_thread_local = threading.local()
+
+def get_thread_display()->display.Display:
+    if not hasattr(_thread_local, 'display'):
+        _thread_local.display = display.Display()
+    return _thread_local.display
 
 def active_window_id()->int:
-	return x.get_focused_window()
+	return get_thread_display().get_input_focus().focus.id
+
+def _window_from_id(window_id: int)->display.drawable.Window:
+	return get_thread_display().create_resource_object('window', window_id)
 
 def window_name(window_id: int)->str:
-	return x.get_window_name(window_id).decode('u8')
+	w=_window_from_id(window_id)
+	name=w.get_wm_name()
+	while not name:
+		w=w.query_tree().parent
+		if w==0: return ""
+		name=w.get_wm_name()
+	return name
 
-def window_class(window_id: int)->tuple[str, ...]:
-	try:
-		result=bytes(x.get_window_property(window_id, b"WM_CLASS")).split(b"\0")
-	except XdoException:
-		return ()
-	assert not result[-1]
-	return tuple(x.decode('u8') for x in result[:-1])
+def window_class(window_id: int)->'tuple[str, ...]':
+	w=_window_from_id(window_id)
+	name=w.get_wm_name()
+	while not name:
+		w=w.query_tree().parent
+		if w==0: return ()
+		name=w.get_wm_name()
+	cls=w.get_wm_class()
+	if cls is None: return ()
+	return cls
