ac70f5d8cfb93a81a3bb78dcf8c7fa2ac8638a87
user202729 <25191436+user202729@users.noreply.github.com>
2023-06-03 09:35:49 +0700

    Switch to ewmh

--- getactivewindow/__init__.py
+++ getactivewindow/__init__.py
@@ -5,36 +5,37 @@ from typing import Union
 import threading
 
 from Xlib import X, display  # type: ignore
+from ewmh import EWMH
 
-_thread_local = threading.local()
+class MyLocal(threading.local):
+	def __init__(self)->None:
+		self.display = display.Display()
+		self.ewmh = EWMH(self.display)
 
-def get_thread_display()->display.Display:
-    if not hasattr(_thread_local, 'display'):
-        _thread_local.display = display.Display()
-    return _thread_local.display
+_thread=MyLocal()
 
 def active_window_id()->int:
-	return get_thread_display().get_input_focus().focus.id
+	return _thread.display.get_input_focus().focus.id
 
 def _window_from_id(window_id: int)->display.drawable.Window:
-	return get_thread_display().create_resource_object('window', window_id)
+	return _thread.display.create_resource_object('window', window_id)
 
 def window_name(window_id: int)->str:
 	w=_window_from_id(window_id)
-	name=w.get_wm_name()
+	name=_thread.ewmh.getWmName(w)
 	while not name:
 		w=w.query_tree().parent
 		if w==0: return ""
-		name=w.get_wm_name()
+		name=_thread.ewmh.getWmName(w)
 	return name
 
 def window_class(window_id: int)->'tuple[str, ...]':
 	w=_window_from_id(window_id)
-	name=w.get_wm_name()
+	name=_thread.ewmh.getWmName(w)
 	while not name:
 		w=w.query_tree().parent
 		if w==0: return ()
-		name=w.get_wm_name()
+		name=_thread.ewmh.getWmName(w)
 	cls=w.get_wm_class()
 	if cls is None: return ()
 	return cls
